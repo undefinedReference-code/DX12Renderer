eb449e4bedc5d5a18816d6451fa18794b2ce663e
arron <841223400@qq.com>
2025-09-19 11:47:44 +0800

    Add MathHelper utility class
    Introduces MathHelper with constants for Pi and Infinity, a Clamp template function, and an Identity4x4 matrix generator. Provides common math utilities for use throughout the project.

---
+++ Common/MathHelper.cpp
@@ -0,0 +1,6 @@
+#include "MathHelper.h"
+#include <float.h>
+#include <cmath>
+
+const float MathHelper::Infinity = FLT_MAX;
+const float MathHelper::Pi = 3.1415926535f;
---
+++ Common/MathHelper.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <Windows.h>
+#include <DirectXMath.h>
+#include <cstdint>
+
+class MathHelper
+{
+public:
+	static const float Pi;
+	static const float Infinity;
+
+	template<typename T>
+	static T Clamp(const T& x, const T& low, const T& high)
+	{
+		return x < low ? low : (x > high ? high : x);
+	}
+
+	static DirectX::XMFLOAT4X4 Identity4x4()
+	{
+		static DirectX::XMFLOAT4X4 I(
+			1.0f, 0.0f, 0.0f, 0.0f,
+			0.0f, 1.0f, 0.0f, 0.0f,
+			0.0f, 0.0f, 1.0f, 0.0f,
+			0.0f, 0.0f, 0.0f, 1.0f);
+
+		return I;
+	}
+};
